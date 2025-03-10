06193e4ad7261f4579284538e2a04ab7f25cdc5b
stpotter16 <stpotter1642@gmail.com>
2025-03-10 09:46:25 -0400

    Parse command line args and pass to handlers

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
     // Parse commands and options
+    if (len(os.Args) != 2) {
+        log.Fatalf("Incorrect number of arguments received. Expected 1, recieved %d", len(os.Args) - 1)
+    }
+    filePath := os.Args[1]
+
     // Initialize the background go function for watching file updates
+
     // Initialize and run the server
     mux := http.NewServeMux()
-    mux.HandleFunc("GET /", indexGet())
+    mux.HandleFunc("GET /", indexGet(filePath))
     http.ListenAndServe(":8080", mux)
 }
 
-func indexGet() http.HandlerFunc {
+func indexGet(_ string) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "Hello, World!")
     }
