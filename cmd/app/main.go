8acfe2bfb68407584be5bab017bd4df86f11d171
stpotter16 <stpotter1642@gmail.com>
2025-03-10 09:37:09 -0400

    Spin up a server in main command

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-    println("Hello, World!")
+    // Parse commands and options
+    // Initialize the background go function for watching file updates
+    // Initialize and run the server
+    mux := http.NewServeMux()
+    mux.HandleFunc("GET /", indexGet())
+    http.ListenAndServe(":8080", mux)
+}
+
+func indexGet() http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, World!")
+    }
 }
