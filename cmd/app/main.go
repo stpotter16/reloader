2bd3ac8eed122aff229489194efc50d89a0d22fe
stpotter16 <stpotter1642@gmail.com>
2025-03-10 10:53:42 -0400

    Use flag parsing for all input options

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type options struct {
+    filePath string
+    port int
+}
+
 func main() {
     // Parse commands and options
-    if (len(os.Args) != 2) {
-        log.Fatalf("Incorrect number of arguments received. Expected 1, recieved %d", len(os.Args) - 1)
-    }
-    filePath := os.Args[1]
+    options := parseOptions()
 
     // Initialize the background go function for watching file updates
 
     // Initialize and run the server
     mux := http.NewServeMux()
-    mux.HandleFunc("GET /", indexGet(filePath))
-    http.ListenAndServe(":8080", mux)
+    mux.HandleFunc("GET /", indexGet(options.filePath))
+    addr := fmt.Sprintf(":%d", options.port)
+    log.Printf("Serving on %s", addr)
+    http.ListenAndServe(addr, mux)
+}
+
+func parseOptions() options {
+    // TODO: Error handling
+    file := flag.String("file", "", "The file to serve")
+    port := flag.Int("port", 8080, "The port to serve the file from on localhost")
+    flag.Parse()
+    return options{
+        filePath: *file,
+        port: *port,
+    }
 }
 
 func indexGet(filePath string) http.HandlerFunc {
@@ -35,6 +51,7 @@ func indexGet(filePath string) http.HandlerFunc {
 func readFile(filePath string) (string, error) {
     data, err := os.ReadFile(filePath)
     if err != nil {
+        log.Printf("Error reading file: %v", err)
         return "", err
     }
     return string(data), nil
